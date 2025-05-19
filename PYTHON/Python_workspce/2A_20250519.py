12dcd634fac7998043bdc6d449af99bedef56306
cholholim <ghfla225@gmail.com>
2025-05-19 13:49:09 +0900

    20250519 파이썬

--- PYTHON/Python_workspce/2A_20250519.py
+++ PYTHON/Python_workspce/2A_20250519.py
@@ -1,126 +1,235 @@
-import os
-# 폴더가 없으면 생성
-folder_path = 'gugudan_EXCEL'
-if not os.path.exists(folder_path):
-    os.makedirs(folder_path)
-import  openpyxl
+import openpyxl
 
 def test1():
-    """구구단 출력하여 엑셀파일로 저장하기
+    """구구단 출력하여 엑셀파일에 저장하기
     """
-    # 엑셀파일 생성
     wb = openpyxl.Workbook()
     ws = wb.create_sheet("구구단")
-
-    # ws['A1'] = '2단'
-    # ws['B1'] = '3단'
-    # ws['C1'] = '4단'
-    # ws['D1'] = '5단'
-    # ws['E1'] = '6단'
-    # ws['F1'] = '7단'
-    # ws['G1'] = '8단'
-    # ws['H1'] = '9단'
-
-    ws['A2'] = "2X1=2"
-    ws['A3'] = "2X2=4"
-    ws['A4'] = "2X3=6"
-    # ...
-    ws['A10'] = "2X9=18"
-
     cols = "ABCDEFGH"
     dan = 2
     for col in cols:
         ws[f"{col}1"] = f"{dan}단"
-        # 구구단 출력
-        for i in range(1, 10):
-         ws[f"{col}{i+1}"] = f"{dan} X {i} = {dan*i}"
-        dan += 1
-    # 엑셀파일 저장
-    wb.save(os.path.join(folder_path,'gugudan.xlsx'))
-    print('엑셀파일 저장 완료')
+        for i in range(1,10):
+            ws[f"{col}{i+1}"] = f"{dan} x {i} = {dan*i}"
+        dan += 1   
+    
+    wb.save("gugudan.xlsx")
+    print("구구단 파일이 저장되었습니다!")
+
 # test1()
 
-def test2():
-    """엑셀파일에서 구구단 읽어오기
-    """
-    # 엑셀파일 열기
-    wb = openpyxl.load_workbook(os.path.join(folder_path,'gugudan.xlsx'))
-    ws = wb['구구단']
 
+def test2():
+    """엑셀 파일의 내용 읽기
+    """
+    wb = openpyxl.load_workbook("gugudan.xlsx")
+    ws = wb["구구단"]
+    
     cols = "ABCDEFGH"
     for col in cols:
-        for i in range(1, 11):
-            print(ws[f"{col}{i}"].value)
+        for i in range(1,11):
+            print(ws[f'{col}{i}'].value)
         print()
+
 # test2()
 
+
+class SoccerPlayer(object):
+    def __init__(self, name, position, back_number):
+        self.name = name
+        self.position = position
+        self.back_number = back_number
+    def change_back_number(self, new_number):
+        self.back_number = new_number
+    def __str__(self):
+        return f"이름은 {self.name}, 포지션은 {self.position}, 등번호는 {self.back_number}"
+
 def test3():
-    pass
+    p1 = SoccerPlayer("홍길동","MF",17)
+    print(f"이름은 {p1.name}, 포지션은 {p1.position}, 등번호는 {p1.back_number}")
+    print(p1.__str__())
+    print(p1)
+
 # test3()
 
-class circle():
+
+class Circle():
     """원을 나타내는 클래스
-    필드: 반지름(redius)
-    메소드: 둘레(get_around, 면적(get_area) 구하기
+    필드: 반지름(radius)
+    메소드: 둘레(get_around), 면적(get_area) 구하기
     """
     def __init__(self, radius):
         self.radius = radius
-        
     def get_around(self):
-        """원의 둘레를 구하는 메소드
-        """
         return 2 * 3.141592 * self.radius
-    
     def get_area(self):
-        """원의 면적을 구하는 메소드
-        """
         return 3.141592 * self.radius ** 2
-    
     def __str__(self):
-        """원의 반지름을 문자열로 반환하는 메소드
-        """
-        return f"원의 반지름: {self.radius}, 둘레: {self.get_around()}, 면적: {self.get_area()}"
+        return f"반지름은 {self.radius}, 둘레는 {self.get_around()}, 면적은 {self.get_area()}"
 
 def test4():
-    """circle 클래스 테스트
-    """
-    c1 = circle(10)
-    c2 = circle(20)
+    c1 = Circle(10)
+    c2 = Circle(20)
     print(c1)
     print(c2)
+
 # test4()
 
+
 class Rectangle():
-    """
-    사각형을 나타내는 클래스
+    """사각형 클래스
     속성: 넓이, 높이
     메소드: 면적, 둘레 구하기
     """
-
     def __init__(self, width, height):
         self.width = width
         self.height = height
-
     def get_area(self):
-        """사각형의 면적을 구하는 메소드
-        """
         return self.width * self.height
-
     def get_around(self):
-        """사각형의 둘레를 구하는 메소드
-        """
-        return 2 * (self.width + self.height)
-
+        return (self.width + self.height) * 2
     def __str__(self):
-        """사각형의 넓이와 높이를 문자열로 반환하는 메소드
-        """
-        return f"사각형의 넓이: {self.get_area()}, 둘레: {self.get_around()}"
+        return f"넓이는 {self.width}, 높이는 {self.height}, 둘레는 {self.get_around()}, 면적은 {self.get_area()}"
 
 def test5():
-    """Rectangle 클래스 테스트
-    """
-    r1 = Rectangle(10, 5)
-    r2 = Rectangle(10, 10)
+    r1 = Rectangle(10,5)
+    r2 = Rectangle(10,10)
     print(r1)
     print(r2)
+
 # test5()
+
+
+# 클래스 변수 vs 인스턴스 변수
+class Person():
+    species = "Homo sapiens"
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def __str__(self):
+        return f"이름은 {self.name}, 나이는 {self.age}세, 종은 {Person.species}"
+
+def test6():
+    p1 = Person("홍길동",10)
+    p2 = Person("홍길순",13)
+    print(p1)
+    print(p2)
+
+# test6()
+
+
+# ******************************
+# 메소드 오버로딩(X) -> 가변인자
+# ******************************
+class Calculator():
+    """2개의 수에 대해서 사칙연산을 수행하는 계산기
+    """
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+    def add(self):
+        return self.num1 + self.num2
+    def add(self, num1, num2):
+        return num1 + num2
+    def sub(self):
+        return self.num1 - self.num2
+    def sub(self, num1, num2):
+        return num1 - num2
+    def mul(self):
+        return self.num1 * self.num2
+    def mul(self, num1, num2):
+        return num1 * num2
+    def div(self):
+        return self.num1 / self.num2
+    def div(self, num1, num2):
+        return num1 / num2
+
+class Calculator2():
+    def __init__(self, *args):
+        # if len(args) > 0:
+        if len(args):
+            self.num1 = args[0]
+            self.num2 = args[1]
+    def add(self, *args):
+        if len(args):
+            return args[0] + args[1]
+        else:
+            return self.num1 + self.num2
+    def sub(self, *args):
+        if len(args):
+            return args[0] - args[1]
+        else:
+            return self.num1 - self.num2
+    def mul(self, *args):
+        if len(args):
+            return args[0] * args[1]
+        else:
+            return self.num1 * self.num2
+    def div(self, *args):
+        if len(args):
+            return args[0] / args[1]
+        else:
+            return self.num1 / self.num2
+
+def test7():
+    num1 = 10
+    num2 = 5
+    c1 = Calculator2(num1, num2)
+    print("{} + {} = {}".format(num1,num2,c1.add()))
+    print("{} - {} = {}".format(num1,num2,c1.sub()))
+    print("{} x {} = {}".format(num1,num2,c1.mul()))
+    print("{} % {} = {}".format(num1,num2,c1.div()))
+    
+    num1 = 20
+    num2 = 10
+    c2 = Calculator2()
+    print("{} + {} = {}".format(num1,num2,c2.add(num1,num2)))
+    print("{} - {} = {}".format(num1,num2,c2.sub(num1,num2)))
+    print("{} x {} = {}".format(num1,num2,c2.mul(num1,num2)))
+    print("{} % {} = {}".format(num1,num2,c2.div(num1,num2)))
+    
+# test7()
+
+
+class Person():
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+    def do_work(self):
+        print("먹는다!")
+    def __str__(self):
+        return f"이름은 {self.name}, 나이는 {self.age}, 성별은 {self.gender}"
+
+class Employee(Person):
+    def __init__(self, name, age, gender, salary, hire_date):
+        super().__init__(name, age, gender)
+        self.salary = salary
+        self.hire_date = hire_date
+    def do_work(self):
+        print("열심히 일한다!")
+    def __str__(self):
+        return f"{super().__str__()}, 봉급은 {self.salary}, 고용일은 {self.hire_date}"
+
+def work(p):
+    p.do_work()
+
+def test8():
+    p1 = Person("김수지", 21, "여성")
+    e1 = Employee("홍길동", 23, "남성", 2500000, "2025-11-02")
+    work(p1)
+    work(e1)
+
+test8()
+
+
+
+
+
+
+
+
+
+
+
+
