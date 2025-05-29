f4d8f54769f5bd9006203fe6250c07a62664c66f
cholholim <ghfla225@gmail.com>
2025-05-29 18:39:07 +0900

    20250529 파이썬

--- PYTHON/Python_workspce/2A_20250529.py
+++ PYTHON/Python_workspce/2A_20250529.py
@@ -1,76 +1,202 @@
-# # db 연결 및 커서 획득, 쿼리 실행
+# ******************************************
+# 회원정보 관리하기
+# ******************************************
 import sqlite3
-#회원 정보관리 프로그램
 def db_setup():
-    conn = sqlite3.connect('2024201019.db', isolation_level=None)
+    # conn = sqlite3.connect("2A.db", isolation_level=None)
+    conn = sqlite3.connect("2A.db")
     cur = conn.cursor()
-    cur.execute('''
-    CREATE TABLE IF NOT EXISTS member (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL,
-        email TEXT,
-        phone TEXT
-    )
-    ''')
+    cur.execute("""
+            CREATE TABLE IF NOT EXISTS member (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                email TEXT,
+                phone TEXT
+            )
+    """)
     return conn
 def insert_member(conn):
     cur = conn.cursor()
-    name = input("회원 이름을 입력하세요: ")
-    email = input("회원 이메일을 입력하세요: ")
-    phone = input("회원 전화번호를 입력하세요: ")
-    param = (name, email, phone)
-    cur.execute('INSERT INTO member (name, email, phone) VALUES (?, ?, ?)', param)
-    print("회원 정보가 등록되었습니다.")
+    name = input("이름: ")
+    email = input("이메일: ")
+    phone = input("전화번호: ")
+    param = (name,email,phone)
+    cur.execute("INSERT INTO member (name,email,phone) VALUES (?,?,?)", param)
+    print("회원 등록 완료!\n")
 def select_member(conn):
     cur = conn.cursor()
-    cur.execute('SELECT * FROM member')
+    cur.execute("SELECT * FROM member")
     for row in cur.fetchall():
-        print(f"ID: {row[0]} | 이름: {row[1]} | 이메일: {row[2]} | 전화번호: {row[3]}")
-    print('회원 조회 완료!\n')
+        print(f"ID:{row[0]} | 이름:{row[1]} | Email:{row[2]} | Phone:{row[3]}")
+    print("회원 조회 완료!\n")
 def update_member(conn):
     cur = conn.cursor()
-    id = input("수정할 회원의 ID를 입력하세요: ")
-    name = input("새로운 이름을 입력하세요: ")
-    email = input("새로운 이메일을 입력하세요: ")
-    phone = input("새로운 전화번호를 입력하세요: ")
-    param = (name, email, phone, id)
-    cur.execute('UPDATE member SET name=?, email=?, phone=? WHERE id=?', param)
-    print("회원 정보가 수정되었습니다.")
+    id = input("수정할 ID: ")    
+    name = input("수정할 이름: ")
+    email = input("수정할 이메일: ")
+    phone = input("수정할 전화번호: ")
+    param = (name,email,phone,id)
+    cur.execute("UPDATE member SET name=?,email=?,phone=? WHERE id=?",param)
+    print("회원 수정 완료!\n")
 def update_member_new(conn):
     pass
 def delete_member(conn):
     cur = conn.cursor()
-    id = input("삭제할 회원의 ID를 입력하세요: ")
-    cur.execute('DELETE FROM member WHERE id=?', (id,))
-    print("회원 정보가 삭제되었습니다.")
-
+    id = input("삭제할 ID: ")
+    param = (id,)
+    obj = cur.execute("DELETE FROM member WHERE id=?", param)
+    if obj.rowcount == 0:
+        print("[❌] 회원 정보가 없습니다!\n")
+    else:
+        print("[⭕] 회원 삭제 완료\n")
+        
 def main():
     conn = db_setup()
-    # 데이터베이스 연결 및 테이블 생성
     while True:
-        print(">>> 회원 정보 관리 프로그램 <<<")
+        print(">>> 회원 관리 프로그램 <<<")
         print("1. 회원 등록")
         print("2. 회원 조회")
         print("3. 회원 수정")
         print("4. 회원 삭제")
         print("5. 종료")
-        choice = input("원하는 작업을 선택하세요: ")
-        # 회원 등록, 조회, 수정, 삭제 선택
-        if choice == '1':
+        num = input("선택[1~5]: ")
+        if num == "1":
             insert_member(conn)
-        elif choice == '2':
+        elif num == "2":
             select_member(conn)
-        elif choice == '3':
+        elif num == "3":
             update_member(conn)
-        elif choice == '4':
+        elif num == "4":
             delete_member(conn)
-        elif choice == '5':
-            print("프로그램을 종료합니다.")
+        elif num == "5":
+            print("프로그램을 종료합니다!")
             break
         else:
-            print("잘못된 선택입니다.")
-        conn.close()
-    insert_member()
-    select_member()
-    update_member()
-    delete_member()
+            print("선택이 잘못되었습니다!")
+    
+    # conn.commit()
+    conn.close()
+
+# main()
+
+
+
+
+
+
+def test1():
+    from urllib.request import urlopen
+    from bs4 import BeautifulSoup
+    
+    html = urlopen("https://www.naver.com")
+    # print(html.read())
+    bs_obj = BeautifulSoup(html.read(),"html.parser")
+    print(bs_obj)
+
+# test1()
+
+
+def test2():
+    from urllib.request import urlopen
+    from bs4 import BeautifulSoup
+    html_str = """
+        <html>
+            <body>
+                <ul>
+                    <li>hello</li>
+                    <li>bye</li>
+                    <li>welcome</li>
+                </ul>
+            </body>
+        </html>
+    """
+    
+    bs_obj = BeautifulSoup(html_str, "html.parser")
+    # print(bs_obj)
+    ul = bs_obj.find("ul")
+    li = bs_obj.find("li")
+    li_all = bs_obj.findAll("li")
+    for item in li_all:
+        print(item.text)
+
+# test2()
+
+def test3():
+    from bs4 import BeautifulSoup
+    html_str = """
+        <html>
+            <body>
+                <ul class="greet">
+                    <li>Hello</li>
+                    <li>Bye</li>
+                    <li>Welcome</li>
+                </ul>
+                <ul class="reply">
+                    <li>ok</li>
+                    <li>no</li>
+                    <li>sure</li>
+                </ul>
+            </body>
+        </html>
+    """
+    bs_obj = BeautifulSoup(html_str, "html.parser")
+    ul_1 = bs_obj.find("ul",{"class":"greet"})
+    
+    ul_list = bs_obj.findAll("ul")
+    for ul in ul_list:
+        li_list = ul.findAll("li")
+        for li in li_list:
+            print(li.text)
+
+# test3()
+
+# find, findAll vs select_one, select
+def test4():
+    from bs4 import BeautifulSoup
+    html_str = """
+        <html>
+            <body>
+                <ul> 
+                    <li><a href="https://www.naver.com">네이버</a></li>
+                    <li><a href="https://www.daum.net">다음</a></li>
+                </ul>
+            </body>
+        </html>
+    """
+    bs_obj = BeautifulSoup(html_str, "html.parser")
+    a_list = bs_obj.findAll("a")
+    for a in a_list:
+        # print(a.text)
+        print(a["href"])
+    a_list2 = bs_obj.select("a")
+    for a in a_list2:
+        print(a["href"])
+# test4()
+
+def test5():
+    from urllib.request import urlopen
+    from bs4 import BeautifulSoup
+    
+    url = "https://www.yes24.com"
+    html = urlopen(url)
+    bs_obj = BeautifulSoup(html.read(),"html.parser")
+    a_tag = bs_obj.select_one("#yesFixCorner > dl > dd > ul.yesCornerLi > li:nth-child(1) > a")
+    
+    # li_list = bs_obj.select("#wMbnSub_001 > div > div.cateTxt > ul > li")
+    # for item in li_list:
+    #     print(item.text)
+    # print("-----------------------------")
+        
+    # h4_list = bs_obj.select("#wMbnSub_018 > div > div.cateTxt > h4")
+    # li_list = bs_obj.select("#wMbnSub_018 > div > div.cateTxt > ul > li")
+    # for item in li_list:
+    #     print(item.text)
+    # for item in h4_list:
+    #     print(item.text)
+    
+    #ulCategoryList > li.cate001
+    li_list = bs_obj.select("#ulCategoryList > li")
+    for item in li_list:
+        print(item.text)
+    
+test5()
